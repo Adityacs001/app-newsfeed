7247123aedf79388062a9f76566ad85599a6eb97
Aditya Kumar Singh <Aditya@hra.gov.ae>
2021-10-19 23:03:24 +0400

    Added Model for News data object from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 
 const App: React.FC = () => {
   return <div>New Feed App</div>;
---
+++ src/model/news.ts
@@ -0,0 +1,19 @@
+export type Source = {
+  id: string;
+  name: string;
+};
+
+
+interface News {
+  id: string;
+  urlToImage: string;
+  title: string;
+  publishedAt: string;
+  description: string;
+  content: string;
+  author: string;
+  source: Source;
+  url: string;
+}
+
+export default News;
